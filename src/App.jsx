802b5e767816dcfac1200d915c0a66a0c8b456d0
RJ Cunanan <rjlugtuc@gmail.com>
2024-11-08 14:14:55 -0800

    Updated state so it now receives data from API call using lifecycle method componentDidMount()

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import { Component } from 'react'
-
-// import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 class App extends Component {
@@ -12,41 +8,37 @@ class App extends Component {
 
     // local state:
     this.state = {
-      name: {firstName: 'RJ', lastName: 'Cunanan'},
-      company: 'ZTM'
+      monsters: [],
     };
+  }
+
+  // Lifecyle method that fetches users from the API the first time this
+  // component "mounts" or gets placed onto the DOM
+  componentDidMount() {
+    // fetch the data from the provided url:
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json()) // convert data returned into a JSON object
+      .then((users) => this.setState(() => {  // update state with new users
+        return {monsters: users}
+      },
+      () => { // Callback that console logs the new state
+        console.log(this.state);
+      }
+    ))
   }
 
   render() {
     return (
       <>
         <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-          <p>
-            {/* Curly braces allows us to insert JS: */}
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
-          </p>
-          <button onClick={() => {
-            this.setState(
-              // Updater function that updates our state with a new name:
-              () => {
-                return {
-                  name: {firstName: 'Andrei', lastName: 'Neaogie'}
-                }
-              }, 
-              // Callback function that prints our new state to the console:
-              () => {
-                console.log(this.state);
-              });  
-            // Update state to a different object with a new name
-          }}>
-            Change Name
-          </button>
+          {/* Cycle through each monster and return an h1 tag with the monster's name */}
+          {this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            );
+          })}
         </div>
       </>
     );
